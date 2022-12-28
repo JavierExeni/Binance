37917f57a48d5dda90e5404737ef835c3809c050
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-28 03:06:30 -0400

    style: add backgroundColor, textColor and fontFamily to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#fcd535",
+        secondary: "#fef6d8",
+        tertiary: "#EAECEF",
+        gray: "#707A8A",
+        lightgray: "#B7BDC6",
+      }),
+      textColor: {
+        primary: "#1E2329",
+        secondary: "#181a20",
+        tertiary: "#474d57",
+        warn: "#f0b90b",
+        gray: "#707A8A",
+        lightgray: "#B7BDC6",
+      },
+    },
+    fontFamily: {
+      BinancePlexRegular: ["BinancePlexRegular", "sans-serif"],
+      BinancePlexSemiBold: ["BinancePlexSemiBold", "sans-serif"],
+      BinancePlexMedium: ["BinancePlexMedium", "sans-serif"],
+      BinancePlexBold: ["BinancePlexBold", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
