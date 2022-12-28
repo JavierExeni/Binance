e444da6239e96571713531c44760fca7f928d1f2
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-28 03:10:39 -0400

    feat: add responsive footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent implements OnInit {
+  closed_1: boolean = false;
+  closed_2: boolean = false;
+  closed_3: boolean = false;
+  closed_4: boolean = false;
+  closed_5: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
