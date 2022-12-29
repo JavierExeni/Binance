41682845c9ccc1e40c58ecacbc357c4e680535c3
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-29 11:41:13 -0400

    feat: add table coin currency

---
+++ src/app/components/table-ccurrency/table-ccurrency.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-ccurrency',
+  templateUrl: './table-ccurrency.component.html',
+  styleUrls: ['./table-ccurrency.component.css']
+})
+export class TableCcurrencyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
