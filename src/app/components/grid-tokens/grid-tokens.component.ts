fd29137dac3d12ddc67998e8cab6d2cdc972abe3
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-29 11:39:42 -0400

    feat: add grid token section

---
+++ src/app/components/grid-tokens/grid-tokens.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-tokens',
+  templateUrl: './grid-tokens.component.html',
+  styleUrls: ['./grid-tokens.component.css']
+})
+export class GridTokensComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
