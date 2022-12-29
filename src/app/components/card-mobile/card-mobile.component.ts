1922332c4fe4f100287241b71d216fe8a17ab930
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-29 11:36:43 -0400

    feat: add card mobile and trade sections

---
+++ src/app/components/card-mobile/card-mobile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-mobile',
+  templateUrl: './card-mobile.component.html',
+})
+export class CardMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/card-trade/card-trade.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-trade',
+  templateUrl: './card-trade.component.html',
+})
+export class CardTradeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
