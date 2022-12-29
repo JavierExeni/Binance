d152682d4a5daa0ebff054b8b6790aef055c9e32
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-29 11:35:55 -0400

    refactor: add components for every section part and banner creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { TableCcurrencyComponent } from './components/table-ccurrency/table-ccurrency.component';
+import { CardTradeComponent } from './components/card-trade/card-trade.component';
+import { CardMobileComponent } from './components/card-mobile/card-mobile.component';
+import { GridTokensComponent } from './components/grid-tokens/grid-tokens.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BannerComponent,
+    SliderComponent,
+    TableCcurrencyComponent,
+    CardTradeComponent,
+    CardMobileComponent,
+    GridTokensComponent
   ],
   imports: [
     BrowserModule,
