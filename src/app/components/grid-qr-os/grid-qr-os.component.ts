6a0b5d0e7aaafb5001e6c05df54eb0d172ca1c22
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-30 03:55:11 -0400

    feat: add grid qr os section

---
+++ src/app/components/grid-qr-os/grid-qr-os.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-qr-os',
+  templateUrl: './grid-qr-os.component.html',
+  styleUrls: ['./grid-qr-os.component.css']
+})
+export class GridQrOsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
