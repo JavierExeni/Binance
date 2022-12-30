b76f2ade49d70a3f9610c5e6e0edbbe55c8292fa
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2022-12-30 03:54:21 -0400

    feat: add card possibility section

---
+++ src/app/components/card-possibility/card-possibility.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-possibility',
+  templateUrl: './card-possibility.component.html',
+  styleUrls: ['./card-possibility.component.css']
+})
+export class CardPossibilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
